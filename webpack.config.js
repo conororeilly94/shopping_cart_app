62297dc10a6d24112658cbdde096c2191875721b
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-03-23 18:30:35 +0000

    webpack-dev fully installed

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/js/index.js",
   output: {
-    path: path.resolve(__dirname, "dist/js"),
-    filename: "bundle.js"
-  }
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js"
+  },
+  devServer: {
+    contentBase: "./dist"
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      filename: "index.html",
+      template: "./src/index.html"
+    })
+  ]
 };
