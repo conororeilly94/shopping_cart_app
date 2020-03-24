3f99f2449e6a9f6465a1817032de5e401f4966df
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-03-24 15:03:26 +0000

    added search view

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@
 // https://forkify-api.herokuapp.com/api/search
 
 import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import { elements } from "./views/base";
 
 /* Global state of the app
  * - Search object
@@ -13,23 +15,25 @@ const state = {};
 
 const controlSearch = async () => {
   // 1. Get query from the view
-  const query = "pizza"; // TODO
+  const query = searchView.getInput();
 
   if (query) {
     // 2. New search object and add to state
     state.search = new Search(query);
 
     // 3. Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
 
     // 4. Search for recipes
     await state.search.getResults();
 
     // 5. Render results on UI
-    console.log(state.search.result);
+    searchView.renderResults(state.search.result);
   }
 };
 
-document.querySelector(".search").addEventListener("submit", e => {
+elements.searchFrom.addEventListener("submit", e => {
   e.preventDefault();
   controlSearch();
 });
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -7,7 +7,7 @@ export default class Search {
   async getResults() {
     try {
       const res = await axios(
-        `https://forkify-api.herokuapp.com/api/search?q=${this.query}`
+        `https://forkify-api.herokuapp.com/api/search?&q=${this.query}`
       );
       this.result = res.data.recipes;
       // console.log(this.result);
