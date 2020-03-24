8b7f6fd5fa72f80672c8a6236a303f90f1d6c291
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-03-24 11:34:02 +0000

    added search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,8 @@
 // Global app controller
 // https://forkify-api.herokuapp.com/api/search
 
-import axios from "axios";
+import Search from "./models/Search";
 
-async function getResults(query) {
-  try {
-    const res = await axios(
-      `https://forkify-api.herokuapp.com/api/search?q=${query}`
-    );
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (error) {
-    alert(error);
-  }
-}
-getResults("pizza");
+const search = new Search("pizza");
+console.log(search);
+search.getResults();
