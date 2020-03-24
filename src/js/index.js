dc37ae31a2b02c4bc777740034547b3ea579c535
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-03-24 11:08:11 +0000

    first api call

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
 // Global app controller
+// https://forkify-api.herokuapp.com/api/search
+
+import axios from "axios";
+
+async function getResults(query) {
+  try {
+    const res = await axios(
+      `https://forkify-api.herokuapp.com/api/search?q=${query}`
+    );
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch (error) {
+    alert(error);
+  }
+}
+getResults("pizza");
