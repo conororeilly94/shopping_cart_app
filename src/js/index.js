e9f55a0846f587a77cd1081adf61a6c24cfa219f
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-03-25 10:17:02 +0000

    added recipe controller

--- src/js/index.js
+++ src/js/index.js
@@ -30,12 +30,18 @@ const controlSearch = async () => {
     searchView.clearResults();
     renderLoader(elements.searchRes);
 
-    // 4. Search for recipes
-    await state.search.getResults();
+    try {
+      // 4. Search for recipes
+      await state.search.getResults();
 
-    // 5. Render results on UI
-    clearLoader();
-    searchView.renderResults(state.search.result);
+      // 5. Render results on UI
+      clearLoader();
+      searchView.renderResults(state.search.result);
+    } catch (err) {
+      alert('Something went wrong');
+      clearLoader();
+    }
+
   }
 };
 
@@ -57,6 +63,31 @@ elements.searchResPages.addEventListener("click", e => {
  * Recipe controller
  */
 
-const r = new Recipe(46956);
-r.getRecipe();
-console.log(r);
+const controlRecipe = async () => {
+  // Get ID from URL
+  const id = window.location.hash.replace('#', '');
+  console.log(id);
+
+  if (id) {
+    //  Prepare UI for changes
+
+    // Create new recipe object
+    state.recipe = new Recipe(id);
+
+    try {
+      // Get recipe data
+      await state.recipe.getRecipe();
+
+      // Calculate servings and times
+      state.recipe.calcTime();
+      state.recipe.calcServings();
+
+      // Render recipe
+      console.log(state.recipe);
+    } catch (err) {
+      alert('Something went wrong');
+    }
+  }
+};
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
