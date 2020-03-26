f5187b4fd7245314d2a65b2236f3a015f92656e8
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-03-26 18:54:08 +0000

    added local storage

--- src/js/index.js
+++ src/js/index.js
@@ -145,9 +145,6 @@ elements.shopping.addEventListener('click', e => {
 /** 
  * Likes Controller
  */
-// TESTING
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumberLikes());
 
 const controlLike = () => {
   if (!state.likes) state.likes = new Likes();
@@ -182,6 +179,20 @@ const controlLike = () => {
   }
   likesView.toggleLikeMenu(state.likes.getNumberLikes());
 };
+
+// Restore liked recipes on page load
+window.addEventListener('load', () => {
+  state.likes = new Likes();
+
+  // Restore likes
+  state.likes.readStorage();
+
+  // Toggle like menu button
+  likesView.toggleLikeMenu(state.likes.getNumberLikes());
+
+  // Render the existing likes
+  state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
 // Handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -1,3 +1,5 @@
+import { throws } from "assert";
+
 export default class Likes {
     constructor() {
         this.likes = [];
@@ -6,12 +8,19 @@ export default class Likes {
     addLike(id, title, author, img) {
         const like = { id, title, author, img };
         this.likes.push(like);
+
+        // Perist data in local storage
+        this.persistData();
+
         return like;
     }
 
     deleteLike(id) {
         const index = this.likes.findIndex(el => el.id === id);
         this.likes.splice(index, 1);
+
+        // Perist data in local storage
+        this.persistData();
     }
 
     isLiked(id) {
@@ -20,5 +29,16 @@ export default class Likes {
 
     getNumberLikes() {
         return this.likes.length;
+    }
+
+    persistData() {
+        localStorage.setItem('likes', JSON.stringify(this.likes));
+    }
+
+    readStorage() {
+        const storage = JSON.parse(localStorage.getItem('likes'));
+
+        // Restoring likes from the localStorage
+        if (storage) this.likes = storage;
     }
 }
