4a2ca0a6cc5a9e22f099d1ced2009a2fdb7a902c
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-03-26 12:03:19 +0000

    update recipe servings

--- src/js/index.js
+++ src/js/index.js
@@ -101,3 +101,20 @@ const controlRecipe = async () => {
 };
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+// Handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+  if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+    // * --> any child
+    // Decrease button is clicked
+    if (state.recipe.servings > 1) {
+      state.recipe.updateServings('dec');
+      recipeView.updateServingsIngredients(state.recipe);
+    }
+  } else if ('.btn-increase, .btn-increase *') {
+    // Increase button is clicked
+    state.recipe.updateServings('inc');
+    recipeView.updateServingsIngredients(state.recipe);
+  }
+  console.log(state.recipe);
+});
